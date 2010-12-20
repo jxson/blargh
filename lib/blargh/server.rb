94391f44d0a111e371b0262a88f2f9cbee3fa3a0
Jason Campbell <jason@borderstylo.com>
2010-12-19 16:26:04 -0800

    setting up the public dir for the server

--- lib/blargh/server.rb
+++ lib/blargh/server.rb
@@ -1,6 +1,7 @@
 module Blargh
   class Server < Sinatra::Base
     set :views, proc { Blargh.config.root + Blargh.config.templates_directory }
+    set :public, proc { Blargh.config.root + Pathname.new('public') }
 
     # GET /posts/:slug
     get Blargh.config.permalink do
