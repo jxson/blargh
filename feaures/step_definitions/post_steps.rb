5c17b91a1692a2d5fcffccd0078057d696e34373
Jason Campbell <jason@borderstylo.com>
2011-01-20 00:45:23 -0800

    a feature finally passes =)

--- feaures/step_definitions/post_steps.rb
+++ feaures/step_definitions/post_steps.rb
@@ -3,13 +3,8 @@ Then /^I should see the post(?:|s):$/ do |table|
     post = Blargh::Post.where(:slug => hash['slug']).first
 
     within("article##{ post.css_id }") do
-
+      page.should have_content(post.title)
+      page.source.should match(post.content)
     end
   end
-  # title = table.hashes['title']
-  # content = table.hashes['content']
-
-
-
-  pending
 end
